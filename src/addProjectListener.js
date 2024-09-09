ece1ca298232d57f0ef036654090d9ffef203b7d
Brendan <103078608+brendanlai01@users.noreply.github.com>
2024-09-09 01:44:48 -0400

    Create addProjectListener.js

---
+++ src/addProjectListener.js
@@ -0,0 +1,46 @@
+import { createProjectLi } from "./createProject";
+
+export default function addProjectListeners(){
+    const addProjectToList = document.querySelector('#add-project');
+    let projectDialog = document.querySelector('.project-dialog');
+    let projectForm = document.querySelector('.project-form');
+    let userProject = document.querySelector('#create-project');
+    const submitProject = document.querySelector('#submit-project');
+    const cancelProject = document.querySelector('#cancel-project');    
+
+    updateHeaderListeners();
+    
+    addProjectToList.addEventListener('click', ()=>{
+        projectDialog.showModal();
+    });
+    
+    submitProject.addEventListener('click', (event)=>{
+        event.preventDefault();
+        if(userProject.value !== ''){
+            createProjectLi(userProject.value);
+            projectForm.reset();
+            projectDialog.close();
+        }else{
+            projectForm.reset();
+            return;
+        }
+    })
+    
+    cancelProject.addEventListener('click', ()=>{
+        projectDialog.close();
+    });
+}
+
+function updateHeaderListeners(){
+    let projectsList = document.querySelectorAll('.projects-list li');
+    let header = document.querySelector('.header h1');
+
+    projectsList.forEach((li)=>{
+        li.addEventListener('click', ()=>{
+            header.textContent = `${li.textContent}`;
+            filterProjTasks(li.textContent);
+        })
+    })
+}
+
+export { addProjectListeners, updateHeaderListeners };
