4e4b4e31c94c6cc5627ab77fb792c2e2e7116aea
Brendan <103078608+brendanlai01@users.noreply.github.com>
2024-09-10 18:05:15 -0400

    add imports and call functions on index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import "./style.css";
 import { addContainerListener, cancelListener, submitTaskListener } from "./addTaskListener.js";
+import { createTests } from './createTask.js';
 import createDefaultHeader from './createHeader.js';
+import addProjectListeners from "./addProjectListener.js";
+import { addSidebarTaskListeners } from "./filterTasks";
 
+
+createDefaultHeader();
+addProjectListeners();
+addSidebarTaskListeners();
 addContainerListener();
 submitTaskListener();
 cancelListener();
+createTests();
