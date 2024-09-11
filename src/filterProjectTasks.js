6af271fbef476fe189e2e2fc8922eba638acf2aa
Brendan <103078608+brendanlai01@users.noreply.github.com>
2024-09-10 23:59:56 -0400

    change item to task in filterprojecttasks

--- src/filterProjectTasks.js
+++ src/filterProjectTasks.js
@@ -2,18 +2,15 @@ import { createTaskNode } from "./createTask.js";
 import { myTasks } from "./createTask.js";
 
 function filterProjTasks(project){
-    let filteredArr = myTasks.filter((item)=> item.project.toUpperCase() === project.toUpperCase());
+    let filteredArr = myTasks.filter((task)=> task.project.toUpperCase() === project.toUpperCase());
     resetDisplay();
     console.log(filteredArr);
-    filteredArr.forEach((item)=>{
-        createTaskNode(item.title, item.desc, item.date, item.priority);
-    })
 }
 
 function resetDisplay(){
     let taskList = document.querySelectorAll('.task-container');
-    taskList.forEach((item)=>{
-        item.remove();
+    taskList.forEach((task)=>{
+        task.remove();
     })
 }
 
