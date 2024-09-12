d86df3c4ccfb086126f0a17ad42f5d43d6177a9f
Brendan <103078608+brendanlai01@users.noreply.github.com>
2024-09-12 04:39:26 -0400

    add initializing functions, listeners, and imports

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import "./style.css";
 import { addContainerListener, cancelListener, submitTaskListener } from "./addTaskListener.js";
-import { createTests } from './createTask.js';
+import { myTasks, displayTasks, updateDataIndex } from './createTask.js';
+import { initProjects, initTasks } from "./storage";
 import createDefaultHeader from './createHeader.js';
 import addProjectListeners from "./addProjectListener.js";
 import { addSidebarTaskListeners } from "./filterTasks";
+import { displayProjects, myProjects } from "./createProject";
+
+if(localStorage.getItem("myTasks") === null) initTasks();
+if(localStorage.getItem("myProjects") === null) initProjects();
+displayTasks(myTasks);
+displayProjects(myProjects);
+updateDataIndex();
 
 
 createDefaultHeader();
@@ -12,4 +20,3 @@ addSidebarTaskListeners();
 addContainerListener();
 submitTaskListener();
 cancelListener();
-createTests();
