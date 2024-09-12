893c782f6d482ece454223e5d25c048101c15b5b
Brendan <103078608+brendanlai01@users.noreply.github.com>
2024-09-12 04:38:22 -0400

    Update createProject.js

--- src/createProject.js
+++ src/createProject.js
@@ -1,4 +1,7 @@
 import  { updateHeader, updateHeaderListeners }  from './addProjectListener.js';
+import { myTasks, setMyTasks } from './createTask.js';
+import { getProjects } from './storage.js';
+import { showAll } from './filterTasks.js';
 
 const expand = document.querySelector('#expand');
 let projectsList = document.querySelectorAll('.projects-list li');
@@ -17,12 +20,14 @@ expand.addEventListener('click', dropdown);
 
 ////
 
+let myProjects = getProjects();
+
 function isDisplayNone(element) {
     return window.getComputedStyle(element).display === "none";
 }
 
-function isDisplayBlock(element){
-    return window.getComputedStyle(element).display === 'block';
+function isDisplayed(element){
+    return window.getComputedStyle(element).display !== 'none';
 }
 
 function addProjectInput(name){
@@ -34,25 +39,90 @@ function addProjectInput(name){
     projSelect.appendChild(newProj);
 }
 
+function createProject(projectName){
+    //save project to myProjects
+    let newProject = projectName;
+    myProjects.push(newProject);
+    
+    overwriteMyProjects();
+    let i = myProjects.indexOf(newProject);
+    let projLi = createProjectLi(projectName);
+    projLi.dataset.index = i;
+    return projLi;
+}
+
 function createProjectLi(text){
     let firstChild = document.querySelector('.projects-list li');
 
-    let newProject = document.createElement('li');
-    newProject.textContent = `${text}`;
-    newProject.style.display = 'block';
+    let newProjectLi = document.createElement('li');
+    let newProjSpan = document.createElement('span');
+    let trashIcon = document.createElement('button');
+    trashIcon.classList.add('delete-project', 'white');
+
+    newProjSpan.textContent = `${text}`;
+    trashIcon.addEventListener('click', ()=>{
+        let index = newProjectLi.dataset.index;
+        newProjectLi.remove();
+        myProjects.splice(index, 1);
+        addDataIndex();
+        overwriteMyProjects();
+        console.log(myProjects);
+        deleteProject(text);
+    })
+    newProjectLi.append(newProjSpan, trashIcon);
+    newProjectLi.style.display = 'flex';
+
     let list = document.querySelector('.projects-list');
-    list.appendChild(newProject);
+    list.appendChild(newProjectLi);
 
     projectsList = document.querySelectorAll('.projects-list li');
-    addProjectInput(newProject.textContent);
+    addProjectInput(newProjectLi.textContent);
 
     if(isDisplayNone(firstChild)){
-        newProject.style.display = 'none'
+        newProjectLi.style.display = 'none'
     }
     
     updateHeaderListeners();
     expand.removeEventListener('click', dropdown);
     expand.addEventListener('click', dropdown);
+
+    return newProjectLi;
 }
 
-export { createProjectLi };
+function deleteProject(project){
+    let header = document.querySelector('.header h1');
+    let projectSelect = document.querySelectorAll('#project-select option');
+    let newMyTasks = myTasks.filter((task)=> task.project !== project);
+    setMyTasks(newMyTasks);
+    localStorage.setItem('myTasks', JSON.stringify(myTasks));
+
+    header.textContent = 'Tasks';
+    showAll();
+    projectSelect.forEach((item)=>{
+        if(item.value === project){
+            item.remove();
+        }
+    })
+}
+
+function displayProjects(projects){
+    projects.forEach((project) => {
+        let i = myProjects.indexOf(projects);
+        let projectLi = createProjectLi(project);
+        projectLi.dataset.index = i;
+    });
+}
+
+function addDataIndex(){
+    let list = document.querySelectorAll('.projects-list li');
+    for(let i = 0; i < myProjects.length; i++){
+        if(list[i].textContent === 'Inbox') continue;
+        list[i].dataset.index = i;
+    }
+}
+
+function overwriteMyProjects(){
+    return localStorage.setItem('myProjects', JSON.stringify(myProjects));
+}
+
+export { createProject, displayProjects, myProjects, addDataIndex };
